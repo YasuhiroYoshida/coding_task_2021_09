33c9d07114b4dba45b609d2c31857fe656c7d159
Yasuhiro Yoshida <892791+YasuhiroYoshida@users.noreply.github.com>
2021-08-03 10:17:12 +0300

    Add request specs for Api::V1::Sizes

--- spec/requests/api/v1/sizes_spec.rb
+++ spec/requests/api/v1/sizes_spec.rb
@@ -1,7 +1,58 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "Api::V1::Sizes", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'Api::V1::Sizes', type: :request do
+  describe 'GET /convert' do
+    context 'when params are valid' do
+      context 'and a matching size exists in the target locale' do
+        it 'returns the matching size' do
+          params = {
+            size: {
+              starting_locale: 'UK',
+              starting_locale_size: '22',
+              target_locale: 'US'
+            }
+          }
+          get api_v1_sizes_convert_path, params: params
+
+          expect(response).to have_http_status(200)
+          expect(JSON.parse(response.body)).to eq({ 'target_locale_size' => '18' })
+        end
+      end
+
+      context 'but a matching size does not exist in the target locale' do
+        it 'returns N/A' do
+          params = {
+            size: {
+              starting_locale: 'UK',
+              starting_locale_size: '99',
+              target_locale: 'US'
+            }
+          }
+          get api_v1_sizes_convert_path, params: params
+
+          expect(response).to have_http_status(200)
+          expect(JSON.parse(response.body)).to eq({ 'target_locale_size' => 'N/A' })
+        end
+      end
+    end
+  end
+
+  context 'when params are invalid' do
+    it 'returns error messages' do
+      params = {
+        size: {
+          starting_locale: 'UK',
+          starting_locale_size: '',
+          target_locale: 'US'
+        }
+      }
+      get api_v1_sizes_convert_path, params: params
+
+      expect(response).to have_http_status(422)
+      expect(JSON.parse(response.body)).to eq({ 'messages' => ["Starting locale size can't be blank",
+                                                               'Starting locale size is not a number'] })
+    end
   end
 end
